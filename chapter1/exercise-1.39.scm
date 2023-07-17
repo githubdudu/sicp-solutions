5f3401ce320745665f4d86f87c98cfe0d6048f8b
dudu-WSL2 <35301543+githubdudu@users.noreply.github.com>
2023-07-17 14:55:33 -0700

    For Section 1.3: Added jupyter version of the solutions. Delete all .scm files.

--- chapter1/exercise-1.39.scm
+++
@@ -1,8 +0,0 @@
-(define (tan-cf x k)
-  (define sqrx (square x))
-  (define (iter k frac)
-    (if (= 1 k) 
-        (/ x (- (* 2 k) 1 frac))
-        (iter (- k 1) (/ sqrx (- (* 2 k) 1 frac)))))
-  (iter k 0))
-(tan-cf (/ 3.14159 4) 100) ; 1.0
